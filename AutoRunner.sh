f75e516b473403200155a9f672fb6acd85d5de7e
BachMinh <bachdangthaiminh@gmail.com>
2026-01-10 22:33:52 +0100

    add autorunner

---
+++ AutoRunner.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+
+# SIGINT trap to kill mlagents and AutoRunner on ^C signal
+control_c(){
+    	pkill mlagents-learn
+	if [ -n "$PID" ]; then
+		kill $PID
+	fi
+	exit
+}
+
+trap control_c SIGINT
+
+while true; do 
+	
+
+	run_id=$(($(find data/server -type f | wc -l) + 1))
+
+
+	# starts a new bash process with command not set to different process so SIGINT trap works
+	timeout --foreground 5h bash -c "
+			trap 'exit' SIGINT
+			conda run --no-capture-output -n mlagents python -u training/linux_train_model.py --run-id $run_id" 
+
+	pkill mlagents-learn # Killing ml agents process
+
+	sleep 10
+
+	git add data/server/*.json
+	
+	if ! git diff --cached --quiet ; then # checks if any file has been added to staging
+		git fetch
+		git pull --rebase 
+		git commit -m "Server Auto-Commit : Added training results from run #$run_id"
+		git push
+		echo "Commited results for run_id $run_id after 5 hours"
+	else 
+		echo "No changes to commit for run_id $run_id"
+	fi
+
+	sleep 10
+done
