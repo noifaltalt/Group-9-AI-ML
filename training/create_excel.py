953477c8b81183d05fdc2e9a5c05c13dd8d57697
BachMinh <bachdangthaiminh@gmail.com>
2026-01-13 10:08:19 +0100

    feat: prediction model

--- training/create_excel.py
+++ training/create_excel.py
@@ -1,19 +1,35 @@
 import glob
 import json
 import pandas as pd
+import os
 
 OUTPUT_XLSX = "data/SCT-data.csv"
 
-dfs = []
+def standardize_run_id(data,fp):
+    hostname = os.path.basename(os.path.dirname(os.path.dirname(fp)))
+    old_id = data["run_id"]
+    id = hostname + "_" + old_id
+    data["run_id"] = id
 
-for fp in glob.glob("data/**/SoccerTwos/*.json"):
-    with open(fp,"r", encoding="utf-8") as f:
-        data = json.load(f)
-    df = pd.json_normalize(data, sep=".")
-    dfs.append(df)
+def standardize_score(data):
+    old = data["efficiency_score"]
+    data["efficiency_score"] = old*120
 
-final_df = pd.concat(dfs, ignore_index=True)
+def main():
+    dfs = []
+    for fp in glob.glob("data/**/SoccerTwos/*.json"):
+        with open(fp, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        standardize_run_id(data,fp)
+        standardize_score(data)
+        df = pd.json_normalize(data, sep=".")
+        dfs.append(df)
 
-final_df.to_csv(OUTPUT_XLSX, index=False)
-print("Done:", OUTPUT_XLSX)
+    final_df = pd.concat(dfs, ignore_index=True)
+
+    final_df.to_csv(OUTPUT_XLSX, index=False)
+    print("Done:", OUTPUT_XLSX)
+
+if __name__ == "__main__":
+    main()
 
